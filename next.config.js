0bd74e935ac9e646d3b278e0f04692debf0d306b
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-06-13 03:04:56 +0300

    [INIT] Project

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            'imgurishafc87a.blob.core.windows.net',
+        ],
+    },
+}
+
+module.exports = nextConfig
